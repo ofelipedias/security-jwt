401c2d5d928ffc9d6e5a8747327c4287d87189d0
Felipe Dias <ofelipedias@gmail.com>
2020-08-19 19:08:52 -0300

    Update JpaRepositoryConfig.java

--- src/main/java/com/spring/security/config/JpaRepositoryConfig.java
+++ src/main/java/com/spring/security/config/JpaRepositoryConfig.java
@@ -1,7 +1,9 @@
 package com.spring.security.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
+@Configuration
 public class JpaRepositoryConfig {
 }
